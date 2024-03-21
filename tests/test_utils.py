a00a5d4cf85adab0bc23e9e8a0e09cb03c3dbdf2
Kelly, Conor <conor.kelly@astrazeneca.com>
2024-03-21 11:40:20 +0000

    Improved reliance of test case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,8 +6,6 @@ def test_read_data_success():
     test_data_paths = ["data/data_group_1.csv", "data/data_group_2.csv"]
     processor = WindTurbineDataProcessor(test_data_paths, "1 day")
 
-    try:
-        processor.read_data()
-        assert True
-    except:
-        assert False, "read_data method failed with valid data paths"
+    df = processor.read_data()
+
+    assert not df.isEmpty()
